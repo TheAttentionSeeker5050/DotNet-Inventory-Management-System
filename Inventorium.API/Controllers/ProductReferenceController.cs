7524fefeda086f7d9e69110642cd14b041aff95e
nicolas <nicolas.castellano.6@hotmail.com>
2024-07-06 16:12:35 -0300

    added empty controllers

---
+++ Inventorium.API/Controllers/ProductReferenceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventorium.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductReferenceController : ControllerBase
+    {
+    }
+}
