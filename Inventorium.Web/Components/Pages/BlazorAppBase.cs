2280a2151ef26deeb4c3bd424ce4641c3e71ff7f
nicolas <nicolas.castellano.6@hotmail.com>
2024-07-13 01:47:04 -0300

    added frontend general app data dependency injection, footer and header navbar

---
+++ Inventorium.Web/Components/Pages/BlazorAppBase.cs
@@ -0,0 +1,22 @@
+﻿namespace Inventorium.Web.Components.Pages
+{ 
+    public interface IBlazorAppBase
+    {
+        // string Title;
+        string getTitle();
+        void setTitle(string title);
+    }
+
+    public class BlazorAppBase : IBlazorAppBase 
+    {
+        // page title
+        public string? Title { get; set; } = "";
+
+        public void setTitle(string title) {
+            if (Title == null) Title = "";
+            else Title = title;
+        }
+
+        public string getTitle() { return Title; }
+    }
+}
--- Inventorium.Web/Program.cs
+++ Inventorium.Web/Program.cs
@@ -1,10 +1,13 @@
 using Inventorium.Web.Components;
+using Inventorium.Web.Components.Pages;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<IBlazorAppBase,BlazorAppBase>();
 
 var app = builder.Build();
 
@@ -23,5 +26,6 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
 
 app.Run();
