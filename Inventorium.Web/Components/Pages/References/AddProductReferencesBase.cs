964b745140b528c7b162082026e946db94383d5d
nicolas <nicolas.castellano.6@hotmail.com>
2024-07-27 18:37:15 -0300

    added empty add product reference page

---
+++ Inventorium.Web/Components/Pages/References/AddProductReferencesBase.cs
@@ -0,0 +1,40 @@
+﻿using Inventorium.Dtos.Dtos;
+using Inventorium.Web.Components.Pages.Items;
+using Inventorium.Web.Services;
+using Inventorium.Web.Services.Interface;
+using Microsoft.AspNetCore.Components;
+
+
+namespace Inventorium.Web.Components.Pages.References
+{
+    public class AddProductReferencesBase : ComponentBase
+    {
+        
+        // Reference service handler
+        [Inject]
+        public IReferenceService ReferenceService { get; set; }
+
+        // Base app metadata handler (title, etc)
+        [Inject]
+        public IBlazorAppBase BlazorAppBase { get; set; }
+
+        // Error message output
+        public string ErrorMessage { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            try
+            {
+                this.BlazorAppBase.SetTitle("Add Product Reference");
+
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
